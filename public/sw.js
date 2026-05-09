3399e866e90326a4d71f5ead4587334fc787a348
joetan79 <joe.lhtan@gmail.com>
2026-05-09 14:26:36 +0800

    Auto sync 2026-05-09 14:26:31 | public/js/app.js,public/sw.js,

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'claudio-v12';
+const CACHE = 'claudio-v13';
 const PRECACHE = ['/', '/css/app.css', '/js/i18n.js', '/js/api.js', '/js/app.js'];
 
 self.addEventListener('install', e => {
